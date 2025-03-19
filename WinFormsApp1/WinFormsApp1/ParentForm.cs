eca154e5573a10cf2115f302600d510992307109
LiksK <144459112+LiksK@users.noreply.github.com>
2025-03-19 15:22:56 +0300

    Add files via upload

---
+++ WinFormsApp1/WinFormsApp1/ParentForm.cs
@@ -0,0 +1,35 @@
+namespace WinFormsApp1
+{
+    public partial class ParentForm : Form
+    {
+        private int openDocuments = 0;
+        public ParentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm newChild = new ChildForm();
+            newChild.Text = newChild.Text + " " + ++openDocuments;
+            newChild.MdiParent = this;
+            newChild.Show();
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WindowCascadeMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+
+        }
+
+        private void WindowTileMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+        }
+    }
+}
